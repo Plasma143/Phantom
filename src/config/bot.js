66e87eb396560f0c75ac27c695b61865ca2c3aca
Plasma143 <lachyh06@gmail.com>
2026-06-13 03:19:20 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Helping TFO",
+        name: "Comepeting with other nerd bots",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: 5, 
       },
     ],
   },
